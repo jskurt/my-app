70f98fbd767a4cf3ff460b01a7640b9d4938b84a
Jisung Lee <jisunglee@Jisungs-MacBook-Air.local>
2023-10-29 09:54:20 +0900

    2

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           1
+          2
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
